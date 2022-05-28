b3715c2338340a71879d91e841d98478d3b19165
Xing <317033640@qq.com>
2022-05-28 18:07:02 +0800

    setup

---
+++ flutter_woo_commerce_getx_learn/android/app/src/main/kotlin/woo_commerce/ducafecat/com/flutter_woo_commerce_getx_learn/MainActivity.kt
@@ -0,0 +1,6 @@
+package woo_commerce.ducafecat.com.flutter_woo_commerce_getx_learn
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_woo_commerce_getx_learn/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_woo_commerce_getx_learn/common/index.dart';
+import 'package:get/get.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
+}
